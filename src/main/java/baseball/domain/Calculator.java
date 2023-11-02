205e463ecba402c8167ea2353e14d311f0d2a010
DSRN0000 <dsrn0000@gmail.com>
2023-11-02 22:29:29 +0900

    feat(baseball): create skeleton code

---
+++ src/main/java/baseball/domain/Calculator.java
@@ -0,0 +1,16 @@
+//package baseball.domain;
+//
+//public class Calculator {
+//    public static final int SHARE_BIRTHDAY = 1003; //클래스 변수이면서 변하지 않는 값 //변하면 안되는 숫자이기 때문에 final 키워드 붙여서 "상수"로 만들었다. //상수는 모두 대문자로 사용하고 _ 사용한다. (underscore)
+//    public static int shareResult = 0; //클래스 변수. 변수 값을 공유. 클래스에서 바로 접근(어디에서 접근하든 값이 같기 때문)
+//
+//    public int result = 0; //인스턴스 변수. 변수값을 공유하지 않음. 만들어놓은 객체로 접근
+//
+//    public Calculator() { //생성자는 첫글자 대문자로 작성
+//    }
+//
+//    public int add(int number1, int number2){ //메소드는 첫글자 소문자로 작성 //aplle_Pie (x) => applePie(o) 자바는 대문자로 구별 _는 파이썬(underscore)
+//        result = number1 + number2;
+//        return result;
+//    }
+//}
